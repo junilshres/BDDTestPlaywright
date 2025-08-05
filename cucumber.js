26359551a27e7b08577fcb434b2a3f1cc5e6d811
junilshres <junilshr@gmail.com>
2025-08-04 22:09:57 -0400

    Modified cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -2,7 +2,8 @@ module.exports = {
   default: {
     require: [
       'ts-node/register',
-      'steps/**/*.steps.ts'
+      'steps/**/*.steps.ts',
+      'steps/hooks.ts'
     ],
     requireModule: ['ts-node/register'],
     format: ['json:test-results/results.json', 'progress'],
